13ced6288af929d5c176de6b5f65db2a921369c2
Sushant Godse <sushant.godse@gmail.com>
2021-01-30 21:52:05 +0530

    my first Commit

---
+++ src/components/Submenu.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Submenu extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Submenu</h1>
+      </div>
+    );
+  }
+}
